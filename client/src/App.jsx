c795067edaaf942c05652078c65783a31c4ebb76
ragava <raghavareddypathi@gmail.com>
2025-03-20 18:38:17 +0530

    added pages to the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
+import { BrowserRouter ,Routes, Route,} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+
 
 function App() {
   
 
   return (
     <>
-      <div className='text-3xl text-red-500 font-bold underline'>hellow</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path='/Projects' element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
        
     </>
   )
