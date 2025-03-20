37c1c9e01de8e7f0af8b06fa49af1c5b006ddede
ragava <raghavareddypathi@gmail.com>
2025-03-20 17:44:10 +0530

    front end intialised reacta and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl text-red-500 font-bold underline'>hellow</div>
+       
+    </>
+  )
+}
+
+export default App
